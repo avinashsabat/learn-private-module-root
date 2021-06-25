8a97b7350100ba6b5d088b798300002ac815b69c
avinashsabat <55778309+avinashsabat@users.noreply.github.com>
2021-06-25 10:35:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "module "s3-webapppp" {
-  source  = "app.terraform.io/nash-terraform-org/s3-webapppp/aws"
-  version = "1.0.0"
-  # insert required variables here
-}"
+  source  = "app.terraform.io/nash-terraform-org/s3-webapp/aws"
   name        = var.name
   region = var.region
   prefix = var.prefix
